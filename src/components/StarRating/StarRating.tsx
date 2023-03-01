020e8c8fc9b70a8d5a868e07cafb585aa4ec1143
a4b11s <a4b11s@gmail.com>
2023-03-01 02:57:51 +0200

    feat: add component StarRating

---
+++ src/components/StarRating/StarRating.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./StarRating.module.css";
+
+const StarRating = (props: { rating: number }) => {
+  const { rating } = props;
+  return (
+    <div>
+      {[...Array(5)].map((item, index) => {
+        if (Math.floor(rating) > index) {
+          return <span className={style.star + " " + style.full}></span>;
+        } else if (rating > index && rating < index + 1) {
+          return <span className={style.star + " " + style.half}></span>;
+        } else {
+          return <span className={style.star + " " + style.empty}></span>;
+        }
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
