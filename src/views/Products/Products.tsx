71c5a77d03c135c2bdfff492056047b1287fae96
a4b11s <a4b11s@gmail.com>
2023-03-03 19:59:33 +0200

    feat:add component "Products"

---
+++ src/views/Products/Products.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { IRootState } from "../../store/store";
+import ProductCard from "../../components/ProductCard/ProductCard";
+
+import classes from "./Products.module.css";
+import Spinner from "../../components/Spinner/Spinner";
+import Alert from "../../components/Alert/Alert";
+
+const Products = () => {
+  const navigate = useNavigate();
+
+  const { data, status, error } = useSelector(
+    (state: IRootState) => state.products
+  );
+
+  const handleClickOnCard = (id: number) => navigate(`/product/${id}`);
+  const handleClickOnBrand = (brand: string) =>
+    navigate(`/products?brand=${brand}`);
+  const handleClickOnCategory = (category: string) =>
+    navigate(`/products?category=${category}`);
+
+  if (status === "fulfilled" && data.length) {
+    return (
+      <div style={{}} className={classes.wrapper}>
+        {data.map((item) => {
+          return (
+            <ProductCard
+              handleClickOnCard={handleClickOnCard}
+              handleClickOnBrand={handleClickOnBrand}
+              handleClickOnCategory={handleClickOnCategory}
+              key={item.id}
+              product={item}
+            />
+          );
+        })}
+      </div>
+    );
+  } else if (status === "pending") {
+    return <Spinner />;
+  } else {
+    return <Alert isOpen={true} message={error} />;
+  }
+};
+
+export default Products;
