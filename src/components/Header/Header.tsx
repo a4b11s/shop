cab39b301ba8a23625b75f7e1ada50aad2cd7345
a4b11s <a4b11s@gmail.com>
2023-03-03 20:01:55 +0200

    feat:add component "Header"

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { routes } from "../../routes/routes";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+const Header = () => {
+  return (
+    <header className={styles.wrapper}>
+      <nav className={styles.nav}>
+        {routes.map(({ path, name, isShow }) => {
+          return isShow ? (
+            <NavLink
+              data-testid="link"
+              key={name}
+              className={styles.link}
+              to={path}
+            >
+              {name}
+            </NavLink>
+          ) : (
+            ""
+          );
+        })}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
