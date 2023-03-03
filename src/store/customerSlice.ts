f96738ac5951f1c13e70e5f0cd08fb0b8144b3c2
a4b11s <a4b11s@gmail.com>
2023-03-03 21:49:26 +0200

    feat!: types action

--- src/store/customerSlice.ts
+++ src/store/customerSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IProduct } from "../models";
 
 interface ICart {
@@ -18,7 +18,7 @@ export const customerSlice = createSlice({
   name: "customer",
   initialState,
   reducers: {
-    addToCart: (state, action) => {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
       const newProduct = action.payload;
       const newProductInCartIndex = state.cart.findIndex(
         (product) => product.product.id === newProduct.id
@@ -29,24 +29,27 @@ export const customerSlice = createSlice({
         state.cart[newProductInCartIndex].count += 1;
       }
     },
-    delFromCart: (state, action) => {
+    delFromCart: (state, action: PayloadAction<number>) => {
       state.cart = state.cart.filter(
         (products) => products.product.id !== action.payload
       );
     },
-    addToFavorite: (state, action) => {
+    addToFavorite: (state, action: PayloadAction<IProduct>) => {
       const newProduct = action.payload;
       const newProductInFavoriteIndex = state.favorite.findIndex(
         (product) => product.id === newProduct.id
       );
       if (newProductInFavoriteIndex !== -1) {
-        state.cart.push(newProduct);
+        state.favorite.push(newProduct);
       }
     },
-    delFromFavorite: (state, action) => {
+    delFromFavorite: (state, action: PayloadAction<number>) => {
       state.favorite = state.favorite.filter(
         (product) => product.id !== action.payload
       );
     },
   },
 });
+
+export const { addToCart, addToFavorite, delFromFavorite, delFromCart } =
+  customerSlice.actions;
