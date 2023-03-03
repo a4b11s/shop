0158d7cc2c8c8abc18c0d96b94e7fe736787051f
a4b11s <a4b11s@gmail.com>
2023-03-03 19:42:48 +0200

    feat:add component "Alert"

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./Alert.module.css";
+import alertIcon from "./alert.svg";
+
+interface IProps {
+  isOpen: boolean;
+  message: string | null;
+}
+
+const Alert = (props: IProps) => {
+  const { isOpen, message } = props;
+  if (isOpen) {
+    return (
+      <div className={classes.wrapper}>
+        <img className={classes.icon} src={alertIcon} alt="alert" />
+        <span>{message}</span>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default Alert;
