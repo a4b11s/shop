0cd34e48c6e04b8af200f5b06201eb77801fa754
a4b11s <a4b11s@gmail.com>
2023-03-03 20:10:07 +0200

    feat!: routes and fetchProducts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout";
 
-function App() {
-  return <></>;
-}
+import { routes } from "./routes/routes";
+
+import "@fontsource/montserrat";
+
+import { fetchProducts } from "./store/productsSlice";
+import { useAppDispatch } from "./store/store";
+
+const App = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {routes.map(({ path, name, component: Component, isIndex }) => {
+            return (
+              <Route
+                index={isIndex}
+                key={name}
+                path={path}
+                element={<Component />}
+              />
+            );
+          })}
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
