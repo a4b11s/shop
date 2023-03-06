f870d2a440d0ee8b0fdf989654a71b50410d893a
a4b11s <a4b11s@gmail.com>
2023-03-06 19:55:03 +0200

    feat: add component Pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import cnBind from "classnames/bind";
+import classes from "./Pagination.module.css";
+
+const cx = cnBind.bind(classes);
+interface IProps {
+  count: number;
+  current: number;
+  onChange?: Function;
+}
+const Pagination = (props: IProps) => {
+  const { count, current, onChange } = props;
+
+  const changePage = (newPage: number) => {
+    onChange && onChange(newPage);
+  };
+  const handleClickOnPageBtn = (value: number) => changePage(value);
+  const handleClickOnPrevBtn = () => changePage(current - 1);
+  const handleClickOnNextBtn = () => changePage(current + 1);
+  return (
+    <div className={classes.wrapper}>
+      <button
+        disabled={current <= 1}
+        onClick={handleClickOnPrevBtn}
+        className={classes.navBtn}
+      >
+        {"❮"}
+      </button>
+      <div className={classes.pages}>
+        {[...Array(count)].map((_, index) => {
+          const pageBtnClassNames = cx({
+            page: true,
+            currentPage: index + 1 === current,
+          });
+          return (
+            <button
+              key={"pagP" + index}
+              onClick={() => {
+                handleClickOnPageBtn(index + 1);
+              }}
+              className={pageBtnClassNames}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+      <button
+        disabled={current >= count}
+        onClick={handleClickOnNextBtn}
+        className={classes.navBtn}
+      >
+        {"❯"}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
