b5dd7274b8993cf96b3b077767d04c1036860576
Artem Bazyl <a4b11s@gmail.com>
2023-03-18 11:13:22 +0200

    fix: add key prop

--- src/components/StarRating/StarRating.tsx
+++ src/components/StarRating/StarRating.tsx
@@ -11,7 +11,8 @@ const StarRating = ({ rating }: IProps) => {
 	return (
 		<div className={classes.wrapper}>
 			{[...Array(5)].map((_, index) => {
-				if (rating >= index && rating <= index + 1) return <Star type={'half'} />;
+				if (rating >= index && rating <= index + 1)
+					return <Star key={index + 'Rating'} type={'half'} />;
 				return (
 					<Star
 						key={index + 'Rating'}
