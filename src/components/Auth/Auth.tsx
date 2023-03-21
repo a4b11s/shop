dcedb5e5357a0e37d8f42b2a81a4fc30a129d6dc
Artem Bazyl <a4b11s@gmail.com>
2023-03-21 17:32:23 +0200

    feat: add component "Auth"

---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import GoogleIcon from '@mui/icons-material/Google';
+
+import Button from '../Button/Button';
+import { useAppDispatch } from '../../store/store';
+import { addUser } from '../../store/customerSlice';
+
+import classes from './Auth.module.css';
+
+interface IProps {
+	onAuth?: () => void;
+}
+
+const Auth = ({ onAuth = () => {} }: IProps) => {
+	const auth = getAuth();
+	const dispatch = useAppDispatch();
+
+	const handleGoogleAuth = () => {
+		const provider = new GoogleAuthProvider();
+
+		signInWithPopup(auth, provider).then((result) => {
+			const credentialFromResult = GoogleAuthProvider.credentialFromResult(result);
+			const { photoURL, email, displayName, uid } = result.user;
+
+			dispatch(
+				addUser({
+					photoURL,
+					email,
+					uid,
+					displayName,
+					accessToken: credentialFromResult?.accessToken || null,
+				})
+			);
+
+			onAuth();
+		});
+	};
+
+	return (
+		<div className={classes.wrapper}>
+			<Button onClick={handleGoogleAuth}>
+				<span className={classes.authBtn}>
+					<GoogleIcon /> Login with google
+				</span>
+			</Button>
+		</div>
+	);
+};
+
+export default Auth;
