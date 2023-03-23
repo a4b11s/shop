2a2a509ef92913736e05e397674e726eaa368599
Artem Bazyl <a4b11s@gmail.com>
2023-03-23 16:31:38 +0200

    feat: add "order" button

--- src/views/Cart/Cart.tsx
+++ src/views/Cart/Cart.tsx
@@ -1,27 +1,34 @@
-import { useSelector } from 'react-redux';
 import React, { useEffect, useState } from 'react';
 
-import { IRootState, useAppDispatch } from '../../store/store';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import ProductListItem from '../../components/ProductListItem/ProductListItem';
+import Price from '../../components/Price/Price';
+import Button from '../../components/Button/Button';
+
 import {
 	changeCountInCart,
 	delFromCart,
 	ICart,
 } from '../../store/customerSlice';
-import ProductListItem from '../../components/ProductListItem/ProductListItem';
-import Price from '../../components/Price/Price';
+import { IRootState, useAppDispatch } from '../../store/store';
 
 import classes from './Cart.module.css';
 
 const sumProductPrice = (product: Array<ICart>): number => {
 	let sum = 0;
+
 	product.forEach((item) => {
 		sum += item.product.price * item.count;
 	});
+
 	return sum;
 };
 
 const Cart = () => {
 	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 	const cart = useSelector((state: IRootState) => state.customer.cart);
 	const [cartPrice, setCartPrice] = useState(0);
 
@@ -39,19 +46,29 @@ const Cart = () => {
 
 	return (
 		<div>
-			{cart.map(({ product, count }) => (
-				<ProductListItem
-					key={product.title + product.id}
-					onChangeCountInCart={handleChangeCountInCart}
-					countInCart={count}
-					product={product}
-					onDelFromCart={handleDelFromCart}
-				/>
-			))}
-
-			<div className={classes.totalPrice}>
-				<span>Total price:</span>
-				<Price price={cartPrice} discountPercentage={0} currency="$" />
+			<div className={classes.cartHeader}>
+				<div className={classes.totalPrice}>
+					<span>Total price:</span>
+					<Price price={cartPrice} discountPercentage={0} currency="$" />
+				</div>
+				<Button
+					onClick={() => {
+						navigate('/order');
+					}}
+				>
+					Order
+				</Button>
+			</div>
+			<div>
+				{cart.map(({ product, count }) => (
+					<ProductListItem
+						key={product.title + product.id}
+						onChangeCountInCart={handleChangeCountInCart}
+						countInCart={count}
+						product={product}
+						onDelFromCart={handleDelFromCart}
+					/>
+				))}
 			</div>
 		</div>
 	);
