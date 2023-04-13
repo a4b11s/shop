0a8c4f365c9521fdebf4f8c2c67c2a250f0a7fd8
Artem Bazyl <a4b11s@gmail.com>
2023-04-13 21:38:55 +0300

    feat!: extract Auth and SignIn forms

---
+++ src/components/AuthForm/AuthForm.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { FormikValues, useFormik } from 'formik';
+
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+import { authorizationValidationSchema } from '../../services/validationSchemas';
+
+interface IProps {
+	handleFormAuthSubmit: (value: FormikValues) => void;
+}
+
+const initialValues = {
+	email: '',
+	password: '',
+};
+
+const AuthForm = (props: IProps) => {
+	const { handleFormAuthSubmit } = props;
+
+	const { errors, touched, ...formik } = useFormik({
+		initialValues: initialValues,
+		validationSchema: authorizationValidationSchema,
+		onSubmit: handleFormAuthSubmit,
+	});
+
+	return (
+		<form onSubmit={formik.handleSubmit}>
+			<Input
+				type="email"
+				label="Email"
+				id="email"
+				onBlur={formik.handleBlur}
+				onChange={formik.handleChange}
+				value={formik.values.email}
+			/>
+			<Input
+				type="password"
+				label="Password"
+				id="password"
+				onBlur={formik.handleBlur}
+				onChange={formik.handleChange}
+				value={formik.values.password}
+			/>
+			<Button type="submit">Login</Button>
+		</form>
+	);
+};
+
+export default AuthForm;
---
+++ src/components/SignInForm/SignInForm.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { FormikValues, useFormik } from 'formik';
+
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+import { authorizationValidationSchema } from '../../services/validationSchemas';
+
+interface IProps {
+	handleFormSignInSubmit: (value: FormikValues) => void;
+}
+
+const initialValues = {
+	email: '',
+	password: '',
+};
+
+const SignInForm = (props: IProps) => {
+	const { handleFormSignInSubmit } = props;
+
+	const { errors, touched, ...formik } = useFormik({
+		initialValues: initialValues,
+		validationSchema: authorizationValidationSchema,
+		onSubmit: handleFormSignInSubmit,
+	});
+
+	return (
+		<form onSubmit={formik.handleSubmit}>
+			<Input
+				type="email"
+				label="Email"
+				id="email"
+				onBlur={formik.handleBlur}
+				onChange={formik.handleChange}
+				value={formik.values.email}
+			/>
+			<Input
+				type="password"
+				label="Password"
+				id="password"
+				onBlur={formik.handleBlur}
+				onChange={formik.handleChange}
+				value={formik.values.password}
+			/>
+			<Button type="submit">SignIn</Button>
+		</form>
+	);
+};
+
+export default SignInForm;
