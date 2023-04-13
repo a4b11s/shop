d9df4dbf35c170ff5ce23bcd13d50b3f29d0af6d
Artem Bazyl <a4b11s@gmail.com>
2023-04-13 21:45:07 +0300

    feat!: add signIn and replace forms on form component

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FormikValues, useFormik } from 'formik';
+import { FormikValues } from 'formik';
 
 import Button from '../Button/Button';
-import Input from '../Input/Input';
-import { authorizationValidationSchema } from '../../services/validationSchemas';
+import AuthForm from '../AuthForm/AuthForm';
+import SignInForm from '../SignInForm/SignInForm';
 
 import { useAppDispatch } from '../../store/store';
 import { addUser } from '../../store/customerSlice';
@@ -17,28 +17,30 @@ import GoogleIcon from '@mui/icons-material/Google';
 
 interface IProps {
 	onAuth?: () => void;
+	onSignIn?: () => void;
 }
 
 const Auth = (props: IProps) => {
-	const { auth } = useAuth();
-	const { onAuth = () => {} } = props;
+	const { onAuth = () => {}, onSignIn = () => {} } = props;
+	const { signIn, auth } = useAuth();
+	const [isSigningIn, setIsSigningIn] = useState(false);
 	const dispatch = useAppDispatch();
-
-	const initialValues = {
-		email: '',
-		password: '',
-	};
-	const { errors, touched, ...formik } = useFormik({
-		initialValues: initialValues,
-		validationSchema: authorizationValidationSchema,
-		onSubmit: handleFormAuthSubmit,
-	});
 
 	function handleFormAuthSubmit(value: FormikValues) {
 		auth(
 			(user) => {
 				dispatch(addUser(user));
 				onAuth();
+			},
+			{ email: value.email as string, password: value.password as string }
+		);
+	}
+
+	function handleFormSignInSubmit(value: FormikValues) {
+		signIn(
+			(user) => {
+				dispatch(addUser(user));
+				onSignIn();
 			},
 			{ email: value.email as string, password: value.password as string }
 		);
@@ -51,33 +53,28 @@ const Auth = (props: IProps) => {
 		});
 	}
 
+	function handleSignInAuthButtonClick() {
+		setIsSigningIn((prevState) => !prevState);
+	}
+
 	return (
 		<div className={classes.wrapper}>
-			<form onSubmit={formik.handleSubmit}>
-				<Input
-					type="email"
-					label="Email"
-					id="email"
-					onBlur={formik.handleBlur}
-					onChange={formik.handleChange}
-					value={formik.values.email}
-				/>
-				<Input
-					type="password"
-					label="Password"
-					id="password"
-					onBlur={formik.handleBlur}
-					onChange={formik.handleChange}
-					value={formik.values.password}
-				/>
-				<Button type="submit">Login</Button>
-			</form>
 			<div>
+				{isSigningIn ? (
+					<AuthForm handleFormAuthSubmit={handleFormAuthSubmit} />
+				) : (
+					<SignInForm handleFormSignInSubmit={handleFormSignInSubmit} />
+				)}
 				<Button onClick={handleGoogleAuthButtonClick}>
 					<span className={classes.authBtn}>
 						<GoogleIcon /> Login with google
 					</span>
 				</Button>
+				<div>
+					<button onClick={handleSignInAuthButtonClick}>
+						{isSigningIn ? 'Login' : 'SignIn'}
+					</button>
+				</div>
 			</div>
 		</div>
 	);
