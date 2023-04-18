0ee9e9a58d0cb0bd442b8ae5a21cb7807746ffaf
Artem Bazyl <a4b11s@gmail.com>
2023-04-19 00:17:03 +0300

    feat: add addressApi service

---
+++ src/services/addressApi.ts
@@ -0,0 +1,9 @@
+export const getCountryArray = () => {
+	return ['Ukrainian', 'Poland', 'Great Britain'];
+};
+export const getCityArray = (country: string) => {
+	return ['Kharkiv', 'Kyiv'];
+};
+export const getAddressArray = (city: string) => {
+	return ['Heroiv Kharkiva', 'Studentska', 'Rybalko'];
+};
