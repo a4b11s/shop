12d3a190ef6326f0c48c9a3d018ea46eab9b00f9
Artem Bazyl <a4b11s@gmail.com>
2023-04-28 20:16:52 +0300

    style: add linebreaks

--- src/services/addressApi.ts
+++ src/services/addressApi.ts
@@ -1,9 +1,11 @@
 export const getCountryArray = () => {
 	return ['Ukrainian', 'Poland', 'Great Britain'];
 };
+
 export const getCityArray = (country: string) => {
 	return ['Kharkiv', 'Kyiv'];
 };
+
 export const getAddressArray = (city: string) => {
 	return ['Heroiv Kharkiva', 'Studentska', 'Rybalko'];
 };
