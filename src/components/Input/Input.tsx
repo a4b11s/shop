6cc0c7f8da764891ac498f07423716921a208206
Artem Bazyl <a4b11s@gmail.com>
2023-04-28 19:30:20 +0300

    feat: add autocomplete

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,9 @@
-import React, { ChangeEvent, InputHTMLAttributes, useState } from 'react';
+import React, {
+	ChangeEvent,
+	InputHTMLAttributes,
+	useRef,
+	useState,
+} from 'react';
 
 import cnBind from 'classnames/bind';
 
@@ -10,6 +15,7 @@ interface IProps extends InputHTMLAttributes<HTMLInputElement> {
 	label: string;
 	errorMessage?: string;
 	value?: string | number;
+	autocomplete?: Array<string>;
 }
 
 const Input = (props: IProps) => {
@@ -18,36 +24,91 @@ const Input = (props: IProps) => {
 		onChange = () => {},
 		value = '',
 		label,
+		autocomplete,
 		...otherProps
 	} = props;
 	const [inputValue, setInputValue] = useState(value);
+	const inputRef = useRef<HTMLInputElement>(null);
 
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setInputValue(e.target.value);
 		onChange(e);
 	};
 
+	const handleAutoCompleteClick = (autocompleteItem: string) => {
+		setInputValue(autocompleteItem);
+
+		if (inputRef.current) {
+			const valueSetter = Object.getOwnPropertyDescriptor(
+				inputRef.current,
+				'value'
+			)?.set;
+			const prototype = Object.getPrototypeOf(inputRef.current);
+			const prototypeValueSetter = Object.getOwnPropertyDescriptor(
+				prototype,
+				'value'
+			)?.set;
+
+			if (valueSetter && valueSetter !== prototypeValueSetter) {
+				prototypeValueSetter?.call(inputRef.current, autocompleteItem);
+			} else {
+				valueSetter?.call(inputRef.current, autocompleteItem);
+			}
+
+			const event = new Event('input', { bubbles: true });
+			inputRef.current.dispatchEvent(event);
+		}
+	};
+
 	return (
-		<div
-			className={cx({
-				inputWrapper: true,
-				inputWrapperError: errorMessage,
-				inputWrapperNotEmpty: value,
-			})}
-		>
-			<label className={classes.label} htmlFor={props.id}>
-				{label}
-			</label>
-			<input
-				onChange={handleChange}
-				value={inputValue}
-				className={classes.input}
-				{...otherProps}
-			/>
-			{errorMessage && (
-				<span className={classes.errorMessage}>{errorMessage}</span>
-			)}
-		</div>
+		<>
+			<div
+				className={cx({
+					inputWrapper: true,
+					inputWrapperError: errorMessage,
+					inputWrapperNotEmpty: value,
+				})}
+			>
+				<label className={classes.label} htmlFor={props.id}>
+					{label}
+				</label>
+				<input
+					ref={inputRef}
+					onChange={handleChange}
+					value={inputValue}
+					className={classes.input}
+					{...otherProps}
+				/>
+				{errorMessage && (
+					<span className={classes.errorMessage}>{errorMessage}</span>
+				)}
+				{autocomplete && (
+					<ul className={classes.autocomplete}>
+						{autocomplete.map((item) => {
+							if (!item.toLowerCase().includes(inputValue.toString().toLowerCase())) {
+								return undefined;
+							}
+
+							if (item.toLowerCase() === inputValue.toString().toLowerCase()) {
+								return undefined;
+							}
+
+							return (
+								<li
+									className={classes.autocompleteItem}
+									onClick={() => {
+										handleAutoCompleteClick(item);
+									}}
+									key={item}
+								>
+									{item}
+								</li>
+							);
+						})}
+					</ul>
+				)}
+			</div>
+		</>
 	);
 };
 
