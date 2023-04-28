f0e0a06607b2bfdc3918ab5fb503c53ac80276be
Artem Bazyl <a4b11s@gmail.com>
2023-04-28 20:43:19 +0300

    feat: add fake api

--- src/services/addressApi.ts
+++ src/services/addressApi.ts
@@ -3,9 +3,16 @@ export const getCountryArray = () => {
 };
 
 export const getCityArray = (country: string) => {
-	return ['Kharkiv', 'Kyiv'];
+	if (country === 'Ukrainian') return ['Kharkiv', 'Kyiv'];
+	if (country === 'Poland') return ['Krakov'];
+	if (country === 'Great Britain') return ['London'];
+	return [''];
 };
 
 export const getAddressArray = (city: string) => {
-	return ['Heroiv Kharkiva', 'Studentska', 'Rybalko'];
+	if (city === 'Kharkiv') return ['Heroiv Kharkiva', 'Studentska', 'Rybalko'];
+	if (city === 'Kyiv') return ['Khreschatyk'];
+	if (city === 'Krakov') return ['Skavinska'];
+	if (city === 'London') return ['Glazbury Rd'];
+	return [''];
 };
